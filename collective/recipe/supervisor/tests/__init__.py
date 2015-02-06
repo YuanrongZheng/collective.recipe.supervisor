edb11d54974860523d40a9fde6d98f475ca5b75d
Moriyoshi Koizumi <mozo@mozo.jp>
2015-02-06 13:52:46 +0900

    Support nested parentheses in processopts.

--- collective/recipe/supervisor/tests/__init__.py
+++ collective/recipe/supervisor/tests/__init__.py
@@ -1,1 +1,10 @@
 # package
+
+def test_suite():
+    import unittest
+    suite = unittest.TestSuite()
+    from . import test_parser
+    suite.addTests(unittest.TestLoader().loadTestsFromModule(test_parser))
+    from . import test_docs 
+    suite.addTests(test_docs.test_suite())
+    return suite
